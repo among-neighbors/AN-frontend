1f96e2d6996f32c3607391341a9f3e7f150853c4
yoonlang <cdt9473@gmail.com>
2022-08-02 16:04:31 +0900

    chore: Update package.json for base setting
    I install Vite with React, Typescript, Prettier, ESLint
    and other dependencies like styled-components etc...

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from './App';
+
+// styles
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
