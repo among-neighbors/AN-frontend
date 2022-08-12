ef543835b8422313165cb8aaa7e5e174d3ca45fa
yoonlang <cdt9473@gmail.com>
2022-08-12 22:09:14 +0900

    feat: Add components of pages

--- client/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import SignPage from './pages/SignPage';
-
-function App() {
-  return <SignPage />;
-}
-
-export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,15 +2,20 @@ import { ThemeProvider } from '@mui/material';
 import theme from './others/colorTheme';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import UserHeader from './components/organisms/Header';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignPage from './pages/SignPage';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <UserHeader />
-      <App />
+      <BrowserRouter>
+        <UserHeader />
+        <Routes>
+          <Route path='/*' element={<SignPage />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>,
 );
