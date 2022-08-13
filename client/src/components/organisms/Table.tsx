4441774b3cdc1cfbcbff900eb938fd2644f71550
yoonlang <cdt9473@gmail.com>
2022-08-14 00:31:31 +0900

    feat: Update table for pages
    I need to make table for mobile version, and treat dynamic pages #4

--- client/src/components/organisms/Table.tsx
+++ client/src/components/organisms/Table.tsx
@@ -12,39 +12,41 @@ interface Column {
   id: 'ID' | 'title' | 'type' | 'writer' | 'date';
   label: string;
   minWidth?: number;
-  align?: 'right';
+  align?: 'center';
   format?: (value: boolean) => string;
 }
 
-const columns: readonly Column[] = [
-  { id: 'ID', label: 'ID', minWidth: 170 },
-  { id: 'title', label: 'title', minWidth: 100 },
-  {
-    id: 'type',
-    label: 'type',
-    minWidth: 170,
-    align: 'right',
-    format: (value: boolean) => {
-      if (value === false) {
-        return '단지';
-      } else {
-        return '라인';
-      }
+const columns = (labels: string[]): Column[] => {
+  return [
+    { id: 'ID', label: `${labels[0]}`, minWidth: 80, align: 'center' },
+    { id: 'title', label: `${labels[1]}`, minWidth: 300 },
+    {
+      id: 'type',
+      label: `${labels[2]}`,
+      minWidth: 90,
+      align: 'center',
+      format: (value: boolean) => {
+        if (value === false) {
+          return '단지';
+        } else {
+          return '라인';
+        }
+      },
     },
-  },
-  {
-    id: 'writer',
-    label: 'writer',
-    minWidth: 170,
-    align: 'right',
-  },
-  {
-    id: 'date',
-    label: 'date',
-    minWidth: 170,
-    align: 'right',
-  },
-];
+    {
+      id: 'writer',
+      label: `${labels[3]}`,
+      minWidth: 110,
+      align: 'center',
+    },
+    {
+      id: 'date',
+      label: `${labels[4]}`,
+      minWidth: 150,
+      align: 'center',
+    },
+  ];
+};
 
 interface Data {
   ID: string;
@@ -54,35 +56,12 @@ interface Data {
   date: string;
 }
 
-function createData(ID: string, title: string, type: boolean, writer: string, date: string): Data {
-  return { ID, title, type, writer, date };
+interface TableProps {
+  labels: string[];
+  rows: Data[];
 }
 
-const rows = [
-  createData('India', 'IN', true, '1324171354', '3287263'),
-  createData('China', 'CN', false, '1403500365', '9596961'),
-  createData('Italy', 'IT', false, '60483973', '301340'),
-  createData('United States', 'US', true, '327167434', '9833520'),
-  createData('Canada', 'CA', false, '37602103', '9984670'),
-  createData('Australia', 'AU', true, '25475400', '7692024'),
-  createData('Germany', 'DE', true, '83019200', '357578'),
-  createData('India', 'IN', true, '1324171354', '3287263'),
-  createData('China', 'CN', false, '1403500365', '9596961'),
-  createData('Italy', 'IT', false, '60483973', '301340'),
-  createData('United States', 'US', true, '327167434', '9833520'),
-  createData('Canada', 'CA', false, '37602103', '9984670'),
-  createData('Australia', 'AU', true, '25475400', '7692024'),
-  createData('Germany', 'DE', true, '83019200', '357578'),
-  createData('India', 'IN', true, '1324171354', '3287263'),
-  createData('China', 'CN', false, '1403500365', '9596961'),
-  createData('Italy', 'IT', false, '60483973', '301340'),
-  createData('United States', 'US', true, '327167434', '9833520'),
-  createData('Canada', 'CA', false, '37602103', '9984670'),
-  createData('Australia', 'AU', true, '25475400', '7692024'),
-  createData('Germany', 'DE', true, '83019200', '357578'),
-];
-
-const StickyHeadTable = () => {
+const StickyHeadTable = ({ labels, rows }: TableProps) => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
@@ -94,14 +73,19 @@ const StickyHeadTable = () => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
-
+  //display: { md: 'block', sm: 'none', xs: 'none' },
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <TableContainer sx={{ maxHeight: 440 }}>
+    <Paper
+      sx={{
+        width: '100%',
+        overflow: 'hidden',
+      }}
+    >
+      <TableContainer sx={{ maxHeight: 600 }}>
         <Table stickyHeader aria-label='sticky table'>
           <TableHead>
             <TableRow>
-              {columns.map((column) => (
+              {columns(labels).map((column) => (
                 <TableCell
                   key={column.id}
                   align={column.align}
@@ -116,7 +100,7 @@ const StickyHeadTable = () => {
             {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
               return (
                 <TableRow hover role='checkbox' tabIndex={-1} key={index}>
-                  {columns.map((column) => {
+                  {columns(labels).map((column) => {
                     const value = row[column.id];
                     return (
                       <TableCell key={column.id} align={column.align}>
--- client/src/pages/NoticePage.tsx
+++ client/src/pages/NoticePage.tsx
@@ -2,6 +2,37 @@ import { useEffect } from 'react';
 import PageHeader from '~/components/organisms/PageHeader';
 import StickyHeadTable from '~/components/organisms/Table';
 
+interface Data {
+  ID: string;
+  title: string;
+  type: boolean;
+  writer: string;
+  date: string;
+}
+
+function createData(ID: string, title: string, type: boolean, writer: string, date: string): Data {
+  return { ID, title, type, writer, date };
+}
+
+const rows = [
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+  createData('3033', '안녕하세요 제목입니다.', true, '홍길동', '2022.08.14'),
+];
+
 const NoticePage = () => {
   useEffect(() => {
     // API로 공지사항 데이터 싹다 끌고와
@@ -11,7 +42,10 @@ const NoticePage = () => {
     <>
       <div className='noticePage'>
         <PageHeader type='notice' />
-        <StickyHeadTable />
+        <StickyHeadTable
+          labels={['공지 ID', '제목', '공지 유형', '작성자', '등록일']}
+          rows={rows}
+        />
       </div>
       <style jsx>{`
         .noticePage {
